485053dfc2ac9569b5f9b90199abe83b55102168
DawsonWhite20 <115961857+DawsonWhite20@users.noreply.github.com>
2026-06-19 14:40:50 -0400

    Added error logging to i2c_master_transmit_receive functionality

--- main/main.c
+++ main/main.c
@@ -2,12 +2,16 @@
 #include "esp_log.h"
 #include "driver/i2c_master.h"
 #include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 // Only two options for scl and sda for esp32 without additional routing
 #define I2C_MASTER_SCL_IO  22
 #define I2C_MASTER_SDA_IO  21
 // Defines first port
 #define I2C_MASTER_PORT_NUM  I2C_NUM_0
+
+static const char *TAG = "MAIN";
 
 i2c_master_bus_handle_t init_i2C_master_port(void) {
     // Configures the master bus
@@ -46,15 +50,30 @@ i2c_master_dev_handle_t init_mpu_device(i2c_master_bus_handle_t bus_handle) {
 
 void app_main(void)
 {
-    uint8_t buffer[6] = {0};
-    uint8_t reg_addr = 0x3B;
-
     // Initializes the master bus
     i2c_master_bus_handle_t main_bus_handle = init_i2C_master_port();
 
     // Initializes the mpu6500 device into the master bus
     i2c_master_dev_handle_t mpu_handle = init_mpu_device(main_bus_handle);
 
-    // Read the mpu register address and specify how many bytes to read and receive
-    ESP_ERROR_CHECK(i2c_master_transmit_receive(mpu_handle, &reg_addr, 1, buffer, 6, -1));
+    while(1) {
+        uint8_t buffer[6] = {0};
+        uint8_t reg_addr = 0x3B;
+
+        // Pass in mpu_handle as well as how many bytes for the esp32 to read and receive 
+        // Catch the return status code into an esp_err_t variable
+        esp_err_t ret = i2c_master_transmit_receive(mpu_handle, &reg_addr, 1, buffer, 6, -1);
+
+        // Check if the transaction succeeded
+        if (ret == ESP_OK) {
+            // Print the 6 raw hex bytes trapped in your array elements
+            ESP_LOGI(TAG, "Success! Raw Bytes: [0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X]", 
+                     buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5]);
+        } else {
+            // This will print if lines are disconnected or target is wrong, without crashing
+            ESP_LOGE(TAG, "I2C Transaction Failed! Error code: %s", esp_err_to_name(ret));
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
 }
