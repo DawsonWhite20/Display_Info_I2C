32a7e9bcdb4c2c0bbc8c9212541cdb89bb6ba999
DawsonWhite20 <115961857+DawsonWhite20@users.noreply.github.com>
2026-06-22 16:56:12 -0400

    Deleted u8g2 library from codebase

--- main/oled_driver.h
+++ main/oled_driver.h
@@ -10,6 +10,6 @@
  *  @param out_device_handle The handle of the initialized OLED display device
  *  @return ESP_OK if successful, otherwise an error code
  */
-esp_err_t oled_init(i2c_master_bus_handle_t bus_handle, i2c_master_dev_handle_t *out_device_handle);
+esp_err_t oled_sensor_init(i2c_master_bus_handle_t bus_handle, i2c_master_dev_handle_t *out_device_handle);
 
 #endif
